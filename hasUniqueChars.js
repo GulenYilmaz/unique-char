a21723ceade0190a97fe71dc029d15129461171d
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2021-10-20 09:31:32 -0400

    has Unique Char

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -13,4 +13,17 @@
 // hasUniqueChars("Bob")
 // // returns true
 
-
+function hasUniqueChars(str){
+    let wordArray =str.split('');
+    let status= true;
+    for (let i =0; i< wordArray.lenght;i ++){
+        for (let j=i+1; j<wordArray.lenght; j ++){
+            if(wordArray[i]=== wordArray[j]){
+           status = false;
+           return status;
+            }
+        }
+    }
+    return status;
+}
+console.log(hasUniqueChars("Monday"))
