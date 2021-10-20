06bfa8d93e32e3282db0b029d2002176f3cf3018
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2021-10-19 21:03:48 -0400

    unique char

---
+++ hasUniqueChars.js
@@ -0,0 +1,16 @@
+// Write your code below
+
+// Given a word, return true if that word contains only unique characters. Return false otherwise.
+
+// For example:
+
+// hasUniqueChars("Monday")
+// // returns true
+// hasUniqueChars("Moonday")
+// // returns false
+// Uppercase and lowercase letters should be considered separately:
+
+// hasUniqueChars("Bob")
+// // returns true
+
+
